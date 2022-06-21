202fbac0e8d0df0c645b1b1f044fb8590ca6d27d
Obianuju Onwuasoanya <onobes007@gmail.com>
2022-06-21 16:38:46 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const myName = 'Obianuju Onwuasoanya';
+const myHeight = 1.63;
+const country = 'Nigeria';
+const details = [`My name is ${myName}, I am ${myHeight}m tall and My country is ${country}.`];
+alert(details);
+
+// Document.write('My name is Obianuju Onwuasoanya. I am 1.63m tall. My country is Nigeria.');
+// print(Document)
